88270132e5509b3b24fcc896da1a54f7db979e51
Alaa Ali <alaa.leithy@gmail.com>
2020-07-11 00:24:06 -0400

    Create middleNode.cpp

---
+++ middleNode.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* first = head;
+        ListNode* second = head;
+        while (second != NULL){
+            second = second->next;
+            if(second == NULL)
+                return first;
+            second = second->next;
+            first = first->next;
+        }
+        return first;
+        
+    }
+};
+
+#https://www.youtube.com/watch?v=wq4OZZ3YLj4
+#https://leetcode.com/problems/middle-of-the-linked-list/
